999d04519c47c3f77f040bf5c4fb2ac981494f14
Kawecki Pawel <kaweckipm@gmail.com>
2024-12-16 14:08:10 +0100

    support ssh (gitlab + github) and partially multiline

--- lua/gitopenweb/init.lua
+++ lua/gitopenweb/init.lua
@@ -1,18 +1,50 @@
+table.unpack = table.unpack or unpack -- 5.1 compatibility
+
 local M = {}
 
-table.unpack = table.unpack or unpack -- 5.1 compatibility
+M.setup = function(opts)
+	print("Options:", opts)
+end
+
+--- @param domain string
+--- @param user string
+--- @param repo string
+--- @return string
+local function build_url(domain, user, repo)
+	if not string.find(domain, "http") then
+		domain = "https://" .. domain
+	end
+	if string.find(domain, "github") then
+		repo = repo .. "/tree"
+	end
+	if string.find(domain, "gitlab") then
+		repo = repo .. "/-/tree"
+	end
+
+	return string.format("%s/%s/%s", domain, user, repo)
+end
+
+--- @return table
+local function get_visual_selection()
+	-- Get the start and end positions of the visual selection
+	local _, start_row, _, _ = unpack(vim.fn.getpos("'<"))
+	local _, end_row, _, _ = unpack(vim.fn.getpos("'>"))
+
+	return {
+		start_row = start_row,
+		end_row = end_row,
+	}
+end
+
 
 M.open = function()
 	local pwd = vim.fn.getcwd()
-	print("PWD:", pwd)
-
 	local origin = vim.fn.system("git remote get-url origin")
 	if origin == "" or string.find(origin, "fatal") then
 		print("Not a git repository")
 		return
 	end
 
-	print("Valid git repository")
 	print("Result:", origin)
 
 	local domain, user, repo, branch
@@ -22,6 +54,7 @@ M.open = function()
 		local parts = string.gmatch(origin, "git@(.*):(.*)/(.*).git")
 
 		domain, user, repo = parts()
+		local baseURL = build_url(domain, user, repo)
 
 		local branch = vim.fn.system("git branch --show-current"):gsub("\n", "")
 		if origin == "" then
@@ -29,60 +62,53 @@ M.open = function()
 			return
 		end
 
-		local relative_path = vim.fn.expand('%:p')
-		local path = string.sub(relative_path, string.len(pwd) + 2)
-		local pos = vim.api.nvim_win_get_cursor(0)
-		local system_command = string.format("open http://%s/%s/%s/tree/%s/%s#L%s", domain, user, repo, branch, path,
-			pos[1])
+		local path = vim.fn.expand('%:p'):sub(string.len(pwd) + 2)
+		local line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+		local system_command = string.format("open %s/%s/%s#L%s", baseURL, branch, path, line)
 
+		print("Command:", system_command)
 
 		vim.fn.system(system_command)
 	elseif string.find(origin, "https://") then
 		print("HTTPS remote repository")
-		local parts = string.gmatch(origin, "git@(.*):(.*)/(.*).git")
-
-		domain, user, repo = parts()
-		vim.fn.system(string.format("open http://%s/%s/%s", domain, user, repo))
+		-- TODO: Implement HTTPS remote repository
 	end
 end
 
-M.open()
+M.open_multiline = function()
+	local pwd = vim.fn.getcwd()
+	local origin = vim.fn.system("git remote get-url origin")
+	if origin == "" or string.find(origin, "fatal") then
+		print("Not a git repository")
+		return
+	end
 
+	local domain, user, repo, branch
 
-M.setup = function(opts)
-	print("Options:", opts)
+	if string.find(origin, "git@") then
+		print("SSH remote repository")
+		local parts = string.gmatch(origin, "git@(.*):(.*)/(.*).git")
+
+		domain, user, repo = parts()
+		local baseURL = build_url(domain, user, repo)
+
+		local branch = vim.fn.system("git branch --show-current"):gsub("\n", "")
+		if origin == "" then
+			print("Not a valid branch")
+			return
+		end
+
+		local path = vim.fn.expand('%:p'):sub(string.len(pwd) + 2)
+		local pos = get_visual_selection()
+		local system_command = string.format("open %s/%s/%s#L%d-L%d", baseURL, branch, path, pos.start_row, pos.end_row)
+
+		print("Command:", system_command)
+
+		vim.fn.system(system_command)
+	end
 end
 
--- local opened = {}
--- local last_window = 0
--- local last_run_definiton = ""
---
--- ---@class Opts
--- ---@field nonverbose boolean
---
--- -- Calculate height of the windows so it's not more than 50% of the screen
--- ---@param window number
--- ---@param size number
--- local function calculate_window_max_height(window, size)
--- 	local w_height = vim.api.nvim_win_get_height(window)
--- 	local half_w_height = math.ceil(w_height / 2)
---
---
--- 	if size > half_w_height then
--- 		size = half_w_height
--- 	elseif size < 5 then
--- 		size = 5
--- 	end
---
--- 	return size
--- end
---
---
---
-
-
-M.cleanup = function()
-	print("Cleanup called:")
-end
+vim.keymap.set('n', '<leader>go', M.open, { noremap = true, silent = true })
+vim.keymap.set('v', '<leader>go', M.open_multiline, { noremap = true, silent = true })
 
 return M
